8825b3204571b331128bbdc76c1fac2b09a00deb
chisun6518 <66107879+chisun6518@users.noreply.github.com>
2024-03-11 18:02:14 +0800

    user

---
+++ shoppingweb/src/main/java/com/shoppingweb/model/UserDAO.java
@@ -0,0 +1,18 @@
+package com.shoppingweb.model;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UserDAO extends JpaRepository<User, String>{
+
+	List<User> findByUserName(String userName);
+	//List<User> findByEmployeeID(String employeeID);
+	
+	User findByEmployeeID(String employeeID);
+	
+	boolean existsByEmployeeID(String employeeID);
+	
+}
