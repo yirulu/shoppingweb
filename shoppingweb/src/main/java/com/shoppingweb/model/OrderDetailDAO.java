c56ff21e52a54830252adb43a6e9edc702543820
A05D730246 <152590426+A05D730246@users.noreply.github.com>
2024-03-12 09:21:21 +0800

    Delete shoppingweb directory
    Delete shoppingweb directory

--- shoppingweb/src/main/java/com/shoppingweb/model/OrderDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.shoppingweb.model;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailDAO extends JpaRepository<OrderDetail,String>{
-	List<OrderDetail> findBysorderId(String sorderId);
-}
