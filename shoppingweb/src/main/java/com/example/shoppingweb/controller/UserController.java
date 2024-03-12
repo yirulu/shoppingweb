da71f5770f7f394c8aad25128123927a98acd870
chisun6518 <66107879+chisun6518@users.noreply.github.com>
2024-03-13 01:53:41 +0800

    Add files via upload

--- shoppingweb/src/main/java/com/example/shoppingweb/controller/UserController.java
+++ shoppingweb/src/main/java/com/example/shoppingweb/controller/UserController.java
@@ -1,58 +1,69 @@
-package com.example.shoppingweb.controller;
+package com.shoppingweb.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.shoppingweb.model.*;
+import com.shoppingweb.model.*;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 	@Autowired
 	UserService srv;
 	
 	//手動執行http://localhost:8080/users
-
+	//查詢全部使用者
 	@GetMapping("/users")
 	public List<User> getAllUsers() {
 		return srv.getAllUsers();
 	}
-//手動執行 http://localhost:8080/users/userName?userName=aa
-	@GetMapping("/users/userName")
-	public List<User> getAllUsersByUserName(@RequestParam String userName) {
+	
+	//手動執行 http://localhost:8080/users/bb
+	//【使用者帳戶+密碼】查使用者個人資料
+	@GetMapping("/users/{userName}")
+	public List<User> getAllUsersByUserName(@PathVariable String userName) {
 		return srv.getAllUsersByUserName(userName);
 	}
-/*
+
+	
+	//手動執行 http://localhost:8080/users/login/zz/zz
+	//【使用者帳戶+密碼】查使用者個人資料
+	@GetMapping("/users/login/{userName}/{password}")
+	public List<User> getAllUsersByUserNameAndPassword(@PathVariable("userName") String userName,@PathVariable("password") String password){
+		return srv.getAllUsersByUserNameAndPassword(userName, password);	
+		
+	}
+
+	/*
 	//手動執行 http://localhost:8080/users/employeeID?employeeID=u001
 	@GetMapping("/users/employeeID")
-	public List<User> getAllUsersByEmployeeID(@RequestParam String employeeID) {
-		return srv.getAllUsersByEmployeeID(employeeID);
+	public List<User> getAllUsersByEmployeeID2(@RequestParam String employeeID) {
+		return srv.getAllUsersByEmployeeID2(employeeID);
 	}
 */
 
-	/*手動執行 http://localhost:8080/users/add
+	/*手動執行 http://localhost:8080/users
 	 * 
- {
-        "employeeID": "u003",
-        "department": null,
-        "name": null,
-        "email": null,
-        "userName": "bb",
-        "password": "bb",
-        "createdAt": null,
-        "createdUser": null,
-        "updatedAt": null,
-        "editUser": null,
-        "enabled": null
-    }
-	 * */
-
-	@PostMapping("/users/add")
+  {
+        "department": "E",
+        "name": "員工11",
+        "email": "gg1@gmail.com.tw",
+        "userName": "gg1",
+        "password": "gg1",
+        "enabled": true
+    }	 * */
+	//新增使用者
+	@PostMapping("/users")
 	public ResponseEntity<String> addUser(@RequestBody User user ){
 		try {
 			srv.addUser(user);
@@ -62,8 +73,9 @@ public class UserController {
 		}			
 	}
 	
-	@PostMapping("/users/update")
-	public ResponseEntity<String> updateUser(@RequestBody  User user  ){
+	//修改使用者訊息
+	@PostMapping("/users/{employeeID}")
+	public ResponseEntity<String> updateUser(@PathVariable String employeeID,@RequestBody  User user  ){
 		try {
 			srv.updateUser(user);
             return ResponseEntity.ok("User updated successfully"); 	
@@ -73,4 +85,17 @@ public class UserController {
                      
 	}	
 	
+	//刪除使用者訊息
+	@DeleteMapping("/users/{employeeID}")
+	public ResponseEntity<String> deleteProductBtId(@PathVariable String employeeID){
+		try {
+			srv.deleteUser(employeeID);
+			return ResponseEntity.ok("User deleted successfully");
+		}catch (Exception e){
+			return ResponseEntity.badRequest().body("Error deleting User:"+e.getMessage());
+		}
+		
+	
+		
+	}	
 }
