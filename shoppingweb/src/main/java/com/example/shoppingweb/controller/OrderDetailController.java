6c99d4fa0d59687fc211ada9516ba2d864e8e080
A05D730246 <152590426+A05D730246@users.noreply.github.com>
2024-03-13 15:56:43 +0800

    Add files via upload

--- shoppingweb/src/main/java/com/example/shoppingweb/controller/OrderDetailController.java
+++ shoppingweb/src/main/java/com/example/shoppingweb/controller/OrderDetailController.java
@@ -1,25 +1,31 @@
 package com.example.shoppingweb.controller;
 
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.shoppingweb.model.OrderDetail;
 import com.example.shoppingweb.model.OrderDetailDAO;
+import com.example.shoppingweb.model.SalesOrderDAO;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/orderDetail")
 public class OrderDetailController {
 	@Autowired
 	OrderDetailDAO dDao;
-
+	@Autowired
+	SalesOrderDAO dao;
 	@GetMapping("/")
 	public ModelAndView gotoIndex() {
 		ModelAndView model = new ModelAndView("detailIndex");
@@ -44,9 +50,12 @@ public class OrderDetailController {
 	
 	//create detail
 	@PostMapping("/addDetail")
-	public List<OrderDetail> createDetail(@ModelAttribute("orderDetail") OrderDetail detail){
+	public List<OrderDetail> createDetail(@RequestBody List<OrderDetail> details){
 		ModelAndView model = new ModelAndView("detailIndex");
-		dDao.save(detail);
+		String soId=dao.findAll().stream().map(t->t.getSorderId()).toList().stream()
+				.max(Comparator.comparing(s->s)).get();
+		System.out.print(soId);
+		details.stream().forEach(d->d.setSorderId(soId));		
 		return getDetail();
 	}
 }
