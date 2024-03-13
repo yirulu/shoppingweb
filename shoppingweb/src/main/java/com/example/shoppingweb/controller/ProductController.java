cea3c03f95769c3fae56c36d64446f2b4872bcd5
Amy Chih <153576873+AmyChih@users.noreply.github.com>
2024-03-13 19:56:34 +0800

    DB referencing version

--- shoppingweb/src/main/java/com/example/shoppingweb/controller/ProductController.java
+++ shoppingweb/src/main/java/com/example/shoppingweb/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.shoppingweb.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.shoppingweb.model.*;
 
+@CrossOrigin("*")
 @RestController
 public class ProductController {
 
