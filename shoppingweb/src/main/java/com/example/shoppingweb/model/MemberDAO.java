12bfcab84ecadaa8a2fda42dc83f6dcb7e47bc45
Choei <choeichiu@gmail.com>
2024-03-17 18:14:40 +0800

    Add files via upload

--- shoppingweb/src/main/java/com/example/shoppingweb/model/MemberDAO.java
+++ shoppingweb/src/main/java/com/example/shoppingweb/model/MemberDAO.java
@@ -1,8 +1,9 @@
 package com.example.shoppingweb.model;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberDAO extends JpaRepository<Member, Integer>{
 	
@@ -10,6 +11,7 @@ public interface MemberDAO extends JpaRepository<Member, Integer>{
     // 使用自動化命名規則進行條件搜尋
     Member findByEmail (String email);
     Member findByName (String name);
+    Member findByEmailAndPassword (String email,String password);
 
     // 使用自動化命名規則進行條件搜尋(多條件)
     //List<Member> findByNameAndAge(String name, Integer age);
@@ -18,5 +20,9 @@ public interface MemberDAO extends JpaRepository<Member, Integer>{
     // 自定義SQL查詢，把 ?1" 帶入 queryByName(String name)
     @Query(value = "select * from members where email = ?1", nativeQuery = true)
     Member queryByEmail(String email);
+    
+    @Modifying
+    @Query("UPDATE Member m SET m.password = :newPassword WHERE m.email = :email")
+    int changePassword(@Param("email") String email, @Param("newPassword") String newPassword);
 
 }
